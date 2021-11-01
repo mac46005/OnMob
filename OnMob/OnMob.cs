582a0bfed1ab1de2750547a4d8a05f2268d2e65d
Marco Preciado <mac46005@gmail.com>
2021-10-31 20:13:09 -0500

    initial commit

---
+++ OnMob/OnMob.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CSharp_Shell
+{
+
+    public class Program 
+    {
+        public static void Main()
+        {
+			
+        }
+    }
+}
